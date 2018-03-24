4e47cf838330e5ca28f1e7721ee2ecb42c2df0c3
Kevin S <kevin@chilicode.ca>
2018-03-23 21:07:40 -0700

    [FIX] Add subdomain for cat service

--- mixins/cat-mixin.js
+++ mixins/cat-mixin.js
@@ -9,35 +9,35 @@ const butt = Jimp.read('images/cat_butt.png'),
 
 const boiHeight = 60;
 
-const MEOW_API = 'https://random.cat/meow';
+const MEOW_API = 'https://aws.random.cat/meow';
 const WOOF_API = 'https://random.dog/woof';
 
 module.exports = (BotBase) =>
 	class CatMixin extends BotBase {
 		constructor() {
 			super();
-			
+
 			this.commands['longcat'] = {
 				helpText: 'Longcat is how long? Alias: {prefix}cat, {prefix}caat, {prefix}caaat, etc.',
 				args: ['#'],
 				method: 'command__longcat'
 			};
-			
+
 			this.commands['meow'] = {
 				helpText: 'Need a cat to cheer you up?',
 				args: [],
 				method: 'command__meow'
 			};
-			
+
 			this.commands['woof'] = {
 				helpText: 'Need a pupper to cheer you up?',
 				args: [],
 				method: 'command__woof'
 			};
-			
+
 			this.addHandler(this.isACat);
 		}
-		
+
 		command__longcat(params, message) {
 			if(!params._ || !params._[0] || isNaN(+params._[0])) {
 				return this.fail(message);
@@ -45,11 +45,11 @@ module.exports = (BotBase) =>
 			let longboi = +params._[0];
 			longboi = longboi > 20 ? 20 : longboi;
 			longboi = longboi <= 1 ? 1 : longboi;
-			
+
 			return Bluebird.all([butt, head, fuzz])
 				.then(([butt, head, fuzz]) => {
 					const cat = Math.floor(Math.random() * (head.bitmap.height / boiHeight)) * boiHeight;
-					
+
 					return CatMixin.newImage(butt.bitmap.width + head.bitmap.width + (fuzz.bitmap.width * longboi), boiHeight, 0x00000000)
 						.then(newImage => {
 							newImage.blit(butt,0,0,0,cat,butt.bitmap.width,boiHeight);
@@ -57,7 +57,7 @@ module.exports = (BotBase) =>
 								newImage.blit(fuzz, butt.bitmap.width + (fuzz.bitmap.width * i), 0, 0, cat, fuzz.bitmap.width, boiHeight);
 							}
 							newImage.blit(head, butt.bitmap.width + (fuzz.bitmap.width * longboi), 0, 0, cat, head.bitmap.width, boiHeight);
-							
+
 							return Bluebird.fromCallback(cb => newImage.getBuffer(Jimp.MIME_PNG, cb));
 						});
 				})
@@ -65,7 +65,7 @@ module.exports = (BotBase) =>
 					return message.channel.send(new BotBase.Discord.Attachment(imgBuffer, 'cat.png'));
 				});
 		}
-		
+
 		command__meow(params, message) {
 			return pr.get(MEOW_API)
 				.then(JSON.parse)
@@ -73,7 +73,7 @@ module.exports = (BotBase) =>
 					return message.channel.send(new BotBase.Discord.Attachment(file, file));
 				});
 		}
-		
+
 		command__woof(params, message, count) {
 			if(count && count > 5) {
 				return message.channel.send('Good heavens! I’m having a bit of trouble getting a doggo for you right now!');
@@ -87,7 +87,7 @@ module.exports = (BotBase) =>
 					}
 				});
 		}
-		
+
 		isACat(message) {
 			if(message.member && message.member.id !== this.bot.user.id) {
 				this.getServerSettings(message)
@@ -103,7 +103,7 @@ module.exports = (BotBase) =>
 				});
 			}
 		}
-		
+
 		static newImage(w, h, color) {
 			return Bluebird.fromCallback(cb => {
 				new Jimp(w, h, color, cb);
