680c460c0be2fa46d5e2187253459bc69cc4a8f1
Kevin S <k3vin.net@gmail.com>
2018-08-13 13:14:37 -0700

    [NEW] Add Sigil mixin (will eventually port this to the bot itself)

---
+++ mixins/sigil-mixin.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const SIGIL_API = 'https://www.k3vin.net/sigils.php?submit=download&phrase=';
+
+module.exports = (BotBase) =>
+	class SigilMixin extends BotBase {
+		constructor() {
+			super();
+			
+			this.commands['sigil'] = {
+				helpText: 'Construct a secret sigil based on an input phrase',
+				args: ['phrase'],
+				method: 'command__sigil',
+				parseParams: false
+			};
+		}
+		
+		async command__sigil(params, message) {
+			const fn = params.replace(/[^a-z]+/g,'_').replace(/(^_+|_+$)/g, '');
+			
+			await message.channel.send(new BotBase.Discord.Attachment(`${SIGIL_API}${encodeURIComponent(params)}`, `${fn}.png`));
+			return message.delete();
+		}
+	};
